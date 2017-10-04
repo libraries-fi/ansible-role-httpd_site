d0b219cfdd9813c0091e8759a89373be4b7b5955
Joonas Kylmälä <joonas.kylmala@kirjastot.fi>
2017-10-04 11:58:21 +0300

    ajsalminen.httpd_site: change sudo to become.

--- tests/test.sh
+++ tests/test.sh
@@ -3,9 +3,9 @@ SITE=test.yml
 ansible-galaxy install ajsalminen.avahi_common
 ansible-galaxy install ajsalminen.avahi_aliases
 ansible-playbook -i $INVENTORY tests/$SITE --syntax-check
-ansible-playbook -i $INVENTORY tests/$SITE --connection=local --sudo
+ansible-playbook -i $INVENTORY tests/$SITE --connection=local --become
   # Rerun for idempotency check.
-ansible-playbook -i $INVENTORY tests/$SITE --connection=local --sudo \
+ansible-playbook -i $INVENTORY tests/$SITE --connection=local --become \
 | grep -q 'changed=0.*failed=0' \
 && (echo 'Idempotence test: pass' && exit 0) \
 || (echo 'Idempotence test: fail' && exit 1)
